aea8a9d51082b81d4ce6fbd6d11b8437a9e80eb9
ifeanyiagujekwu <60798487+ifeanyiagujekwu@users.noreply.github.com>
2020-05-29 05:13:45 -0700

    attaching methods t the buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,44 @@
 import { Component } from '@angular/core';
 import * as moment from 'moment';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent { 
   title = 'my-app';
-  date = moment().format("MMM Do YY").toString();
+  public date = moment().toObject();
+  public n: number = 0;
   
+  tomorrow(n: string){
+    
+    var dayWrapper = moment(this.date);
+    var n = dayWrapper.add((this.n++), 'days').toString();
+    console.log(n);
+    
+  }
+  yesterday(n: string){
+    
+    var dayWrapper = moment(this.date);
+    var n = dayWrapper.subtract((this.n++), 'days').toString();
+    console.log(n);
+    
+  }
+  next_week(n: string){
+    
+    var dayWrapper = moment(this.date);
+    var n = dayWrapper.add((this.n+1), 'weeks').toString();
+    console.log(n);
+    
+  }
+  last_week(n: string){
+    
+    var dayWrapper = moment(this.date);
+    var n = dayWrapper.subtract((this.n+1), 'weeks').toString();
+    console.log(n);
+    
+  }
 
 }
